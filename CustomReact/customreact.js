3e212e28c3f37bbf1dcf776486c246071ab37301
ash-gth <ashisharma9318@gmail.com>
2025-05-21 23:31:45 +0530

    some file changes

---
+++ CustomReact/customreact.js
@@ -0,0 +1,17 @@
+import { Children } from "react"
+
+
+// const reactElement = 
+// {
+//     type : 'a',
+//     props: {
+//         href : "http:google.com",
+//         target: '_blank'
+//         },
+//     Children: 'click me to visit google'
+// }
+
+
+// const mainContainer = document.getElementById('root').render
+
+// CustomRender(reactElement, mainContainer)
